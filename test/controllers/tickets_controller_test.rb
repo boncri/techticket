b8b330f0e72a9789d8b8e1aac5437f661fac8a8f
Cristiano Boncompagni <cristiano.boncompagni@intersail.it>
2014-07-30 03:17:13 +0200

    Test fixes

--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -6,37 +6,43 @@ class TicketsControllerTest < ActionController::TestCase
   end
 
   test "should get index" do
-    get :index
+    get :index, customer_id: @ticket.customer.id
     assert_response :success
     assert_not_nil assigns(:tickets)
+    assert_not_nil assigns(:customer)
   end
 
   test "should get new" do
-    get :new
+    get :new, customer_id: @ticket.customer.id
     assert_response :success
+    assert_not_nil assigns(:ticket)
+    assert_not_nil assigns(:customer)
   end
 
   test "should create ticket" do
     assert_difference('Ticket.count') do
-      post :create, ticket: { customer_id: @ticket.customer_id, description: @ticket.description, quantity: @ticket.quantity, ticket_date: @ticket.ticket_date }
+      post :create, customer_id: @ticket.customer.id, ticket: { customer_id: @ticket.customer.id, description: @ticket.description, quantity: @ticket.quantity, ticket_date: @ticket.ticket_date }
     end
-
-    assert_redirected_to ticket_path(assigns(:ticket))
+    
+    assert_not_nil assigns(:ticket)
+    assert_redirected_to customer_tickets_path(assigns(:customer))
   end
 
   test "should show ticket" do
     get :show, id: @ticket
     assert_response :success
+    assert_not_nil assigns(:ticket)
   end
 
   test "should get edit" do
     get :edit, id: @ticket
     assert_response :success
+    assert_not_nil assigns(:ticket)
   end
 
   test "should update ticket" do
-    patch :update, id: @ticket, ticket: { customer_id: @ticket.customer_id, description: @ticket.description, quantity: @ticket.quantity, ticket_date: @ticket.ticket_date }
-    assert_redirected_to ticket_path(assigns(:ticket))
+    patch :update, id: @ticket, ticket: { customer_id:@ticket.customer.id, description: @ticket.description, quantity: @ticket.quantity, ticket_date: @ticket.ticket_date }
+    assert_redirected_to customer_tickets_path(assigns(:ticket).customer)
   end
 
   test "should destroy ticket" do
@@ -44,6 +50,6 @@ class TicketsControllerTest < ActionController::TestCase
       delete :destroy, id: @ticket
     end
 
-    assert_redirected_to tickets_path
+    assert_redirected_to customer_tickets_path(assigns(:ticket).customer)
   end
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,15 +1,11 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
   should validate_presence_of :name
 
   should have_many :tickets
 
-  test "Customer buys 10 tickets, then use 10 tickets, must have tickets_balance = 0" do
+  test "Customer buys 10 tickets and uses 10 tickets, then must have tickets_balance = 0" do
     c = Customer.create(name: 'Test')
     c.tickets.create(description:'A',quantity: 10, buy: true)
     c.tickets.create(description:'A',quantity:10, buy:false)
@@ -17,10 +13,13 @@ class CustomerTest < ActiveSupport::TestCase
     assert_equal 0, c.tickets_balance
   end
 
-  test "Customer buys 10 tickets, then use 5 tickets, must have tickets_balance = 5" do
+  test "Customer buys 20 tickets and uses 15 tickets in different times, then at the end must have tickets_balance = 5" do
     c = Customer.create(name: 'Test')
     c.tickets.create(description:'A',quantity: 10, buy: true)
     c.tickets.create(description:'A',quantity:5, buy:false)
+    c.tickets.create(description:'A',quantity:5, buy:true)
+    c.tickets.create(description:'A',quantity:10, buy:false)
+    c.tickets.create(description:'A',quantity:5, buy:true)
 
     assert_equal 5, c.tickets_balance
   end
